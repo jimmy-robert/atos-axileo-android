dc89c92a08f800eedaea978dda2f20788d976733
jimmy-robert <jimmy@robert.cloud>
2021-04-27 16:22:17 +0200

    Handle foreground notifications

--- app/src/main/java/cloud/robert/mcumovies/app/McuApp.kt
+++ app/src/main/java/cloud/robert/mcumovies/app/McuApp.kt
@@ -1,7 +1,11 @@
 package cloud.robert.mcumovies.app
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.HiltAndroidApp
 
@@ -14,6 +18,16 @@ class McuApp : Application() {
             if (it.isSuccessful) {
                 Log.i("MCU Movies", "FCM token ${it.result}")
             }
+        }
+
+        // Create main channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("main", "Main channel", importance)
+            channel.description = "Channel principal"
+            NotificationManagerCompat
+                .from(this)
+                .createNotificationChannel(channel)
         }
     }
 }
--- app/src/main/java/cloud/robert/mcumovies/services/McuMessagingService.kt
+++ app/src/main/java/cloud/robert/mcumovies/services/McuMessagingService.kt
@@ -1,7 +1,13 @@
 package cloud.robert.mcumovies.services
 
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.os.bundleOf
+import androidx.navigation.NavDeepLinkBuilder
+import cloud.robert.mcumovies.R
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class McuMessagingService : FirebaseMessagingService() {
     override fun onNewToken(newToken: String) {
@@ -9,5 +15,29 @@ class McuMessagingService : FirebaseMessagingService() {
 
         // todo: save token to target specific device later
         Log.i("MCU Movies", "FCM new token: $newToken")
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        val movieId = message.data["movieId"]?.toInt() ?: 0
+
+        val pendingIntent = NavDeepLinkBuilder(this)
+            .setGraph(R.navigation.main_navigation)
+            .setDestination(R.id.movieFragment)
+            .setArguments(bundleOf("movieId" to movieId))
+            .createPendingIntent()
+
+        val notification = NotificationCompat.Builder(this, "main")
+            .setContentTitle(message.notification?.title)
+            .setContentText(message.notification?.body)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+
+        NotificationManagerCompat
+            .from(this)
+            .notify(movieId, notification)
     }
 }
